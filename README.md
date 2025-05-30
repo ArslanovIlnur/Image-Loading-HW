eed9bdb87260139ffcc2e1008b64a121f5c8bf2f
ArslanovIlnur <jalayak@rambler.ru>
2025-05-30 15:47:31 +0300

    HW

---
+++ README.md
@@ -0,0 +1,3 @@
+## Скриншоты
+![1](assets/Screenshot_20250530_152700.png)
+![2](assets/Screenshot_20250530_152743.png)
--- app/src/main/java/com/example/myapplication/MainActivity.kt
+++ app/src/main/java/com/example/myapplication/MainActivity.kt
@@ -1,29 +1,31 @@
 package com.example.myapplication
 
-import android.app.DownloadManager
 import android.content.Context
-import android.content.Intent
-import android.net.Uri
 import android.os.Bundle
-import android.os.Environment
+import android.view.ViewGroup
+import android.widget.ImageView
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.compose.foundation.clickable
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
-import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.unit.dp
-import androidx.compose.ui.unit.sp
-import androidx.core.content.ContextCompat
+import androidx.compose.ui.viewinterop.AndroidView
+import coil.compose.rememberImagePainter
+import coil.request.ImageRequest
+import coil.size.Scale
+import com.bumptech.glide.Glide
+import com.bumptech.glide.request.RequestOptions
 import com.example.myapplication.ui.theme.MyApplicationTheme
-import java.io.File
-
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -35,56 +37,69 @@ class MainActivity : ComponentActivity() {
                     color = MaterialTheme.colorScheme.background
                 ) {
                     Column {
-                        Text(
-                            text = "Start",
-                            fontSize = 20.sp,
-                            modifier = Modifier.clickable { startService("Started") }
+                        // Загрузка изображения с помощью Glide
+                        GlideImage(
+                            url = "https://images.unsplash.com/photo-1748280816879-89b9ca0d4aa0?q=80&w=1960&auto=format&fit=crop&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
+                            width = 300, // Укажите желаемую ширину
+                            height = 500 // Укажите желаемую высоту
                         )
+
                         Spacer(modifier = Modifier.height(30.dp))
-                        Text(
-                            text = "Stop",
-                            fontSize = 20.sp,
-                            modifier = Modifier.clickable { stopService() }
-                        )
+
+                        // Загрузка изображения по валидному URL с помощью Coil
+                        LoadImage("https://images.unsplash.com/photo-1744539982356-336fac93f4b7?q=80&w=1972&auto=format&fit=crop&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D")
+
                         Spacer(modifier = Modifier.height(30.dp))
-                        Text(
-                            text = "Download",
-                            fontSize = 20.sp,
-                            modifier = Modifier.clickable { download() }
-                        )
+
+                        // Загрузка изображения по невалидному URL с помощью Coil
+                        LoadImage("https://images.unsplash.com/photo-1756-336fac93f4b7?q=80&w=1972&auto=format&fit=crop&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D")
                     }
                 }
             }
         }
     }
 
-    private fun download() {
-        val fileUrl = "http://speedtest.ftp.otenet.gr/files/test10Mb.db"
-        val fileName = "test10Mb.db"
+    @Composable
+    fun LoadImage(url: String) {
+        val painter = rememberImagePainter(
+            ImageRequest.Builder(LocalContext.current)
+                .data(url)
+                .error(R.drawable.ic_error) // Укажите изображение ошибки
+                .placeholder(R.drawable.ic_placeholder) // Укажите изображение загрузки
+                .scale(Scale.FILL)
+                .build()
+        )
 
-        val request = DownloadManager.Request(Uri.parse(fileUrl))
-            .setTitle(fileName)
-            .setDescription("Downloading")
-            .setNotificationVisibility(DownloadManager.Request.VISIBILITY_VISIBLE)
-            .setDestinationInExternalPublicDir(Environment.DIRECTORY_DOWNLOADS, fileName)
-
-        val downloadManager = getSystemService(Context.DOWNLOAD_SERVICE) as DownloadManager
-        downloadManager.enqueue(request)
-    }
-
-    private fun startService(input: String) {
-        val myServiceIntent = Intent(this, MyForegroundService::class.java)
-        myServiceIntent.putExtra(Constants.inputExtra, input)
-        ContextCompat.startForegroundService(this, myServiceIntent)
-    }
-
-    public fun stopService() {
-        val serviceIntent = Intent(this, MyForegroundService::class.java)
-        stopService(serviceIntent)
+        Box(
+            modifier = Modifier
+                .fillMaxWidth()
+                .height(200.dp) // Установите желаемую высоту
+        ) {
+            Image(
+                painter = painter,
+                contentDescription = null,
+                modifier = Modifier.fillMaxSize() // Занимает всю ширину и высоту Box
+            )
+        }
     }
 
     @Composable
-    fun Test(name: String, modifier: Modifier = Modifier) {
-    }
+    fun GlideImage(url: String, width: Int, height: Int, modifier: Modifier = Modifier) {
+        val context = LocalContext.current
 
+        AndroidView(
+            factory = { ctx: Context ->
+                ImageView(ctx).apply {
+                    layoutParams = ViewGroup.LayoutParams(width, height) // Устанавливаем размеры
+                    scaleType = ImageView.ScaleType.CENTER_CROP
+                    Glide.with(ctx)
+                        .load(url)
+                        .apply(RequestOptions().placeholder(R.drawable.ic_placeholder).error(R.drawable.ic_error))
+                        .into(this)
+                }
+            },
+            modifier = modifier
+        )
+    }
 }
+
